1306ec0872905dd917d392f37644c635af407b9f
Keshav Callychurn <kcallychurn@myseneca.ca>
2025-01-12 17:30:08 -0500

    setup routes

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -9,7 +10,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Bhendchood Jubin!!!a</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="./root/tabs/workout"> Workout</Link>
+      <Link href="/root/tabs/profile"> Profile</Link>
+      
+
     </View>
   );
 }
