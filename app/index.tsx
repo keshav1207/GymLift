fe5df89ed071a3a0e2d4e479a4c74b7f4a2c3042
Keshav Callychurn <kcallychurn@myseneca.ca>
2025-01-15 21:15:32 -0500

    Complete Design and styling of sign-in page

--- app/index.tsx
+++ app/index.tsx
@@ -8,8 +8,10 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 10,
       }}
     >
+      <Text style={{ marginBottom: 20, fontWeight: "bold", fontSize: 20 }}> Welcome to GymLift!</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="./root/tabs/workout"> Workout</Link>
       <Link href="/root/tabs/profile"> Profile</Link>
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,105 @@
-      import { View, Text } from 'react-native'
-      import React from 'react'
+import React from 'react'
+import { Text, StyleSheet, ScrollView, Image, Dimensions, TouchableOpacity, View} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
       
+
+const { height } = Dimensions.get('window');
       const signIn = () => {
         return (
-          <View>
-            <Text>sign-in</Text>
-          </View>
+          <SafeAreaView  style={styles.container}>
+            <ScrollView style={styles.sview} >
+
+              <Image source={require('../assets/images/mainPhoto.jpeg')}  style={styles.image} />
+
+              <View className='welcome' style={styles.welcome}>
+                <Text > Welcome to GymLift!</Text>
+                <Text style={{ fontWeight: "bold", fontSize: 18 }}> Let's Get You Closer To </Text>
+                <Text style={{ fontWeight: "bold", fontSize: 18 }}>  Your Fitness Goals</Text>
+                <Text style={styles.text}>Log In with Google</Text>
+
+              </View>
+
+              
+             <TouchableOpacity>
+              <View className='loginBtn'style={styles.loginBtn}>
+              
+                <Image  style={{ width: 25, height: 25 }} source={require('../assets/images/googleIcon.png')}/>
+                <Text> Continue With Google</Text>
+
+                </View>
+                </TouchableOpacity>
+              
+              
+
+            </ScrollView>
+           
+          </SafeAreaView>
         )
+      };
+
+      const styles = StyleSheet.create({
+        container: {
+          backgroundColor: "white",
+          flex: 1,
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+         
+          
+      },
+
+      text: {
+        marginTop: 20,
+        
+        
+      },
+
+      sview: {
+        width: '100%',
+          flex: 1,
+          padding: 20,
+          
+          
+      },
+
+      image:{
+        width: '100%',
+        height: height * 0.6 ,
+        resizeMode: 'cover',
+        borderRadius: 10,
+      },
+
+      welcome: {
+        marginTop: 20,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: 3,
+
+      },
+
+      loginBtn: {
+        marginTop: 50,
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10,
+        justifyContent: 'center',
+        padding: 10,
+        width: 370,
+
+        //Add box show style
+       backgroundColor: 'white', 
+        borderRadius: 20,
+        shadowColor: '#000', 
+        shadowOffset: { width: 0, height: 4 }, 
+        shadowOpacity: 0.1, 
+        shadowRadius: 10, 
+          elevation: 5
+      
+
       }
+    
+    
+    });
       
       export default signIn
