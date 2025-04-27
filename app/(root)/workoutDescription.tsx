e59e61d464d6b610a827d25df040a4ef8f20e786
Keshav Callychurn <keshav0799@gmail.com>
2025-04-26 20:24:24 -0400

    Create basic workoutDescription page
    This will be use later to display workout details with all sets and exercises

---
+++ app/(root)/workoutDescription.tsx
@@ -0,0 +1,46 @@
+import { useLocalSearchParams } from 'expo-router';
+import { SafeAreaView, Text, Image, View, StyleSheet, ScrollView } from 'react-native';
+
+const WorkoutDescription = () => {
+
+  // Extract the query parameters from the URL
+  const { name} = useLocalSearchParams();
+
+ 
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{name}</Text>
+          
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f9f9f9',
+    padding: 20,
+  },
+  scrollView: {
+    paddingBottom: 20,
+  },
+  textContainer: {
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 8,
+  },
+ 
+ 
+});
+
+export default WorkoutDescription;
